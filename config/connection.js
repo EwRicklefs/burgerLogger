b351c703c64a4be237cab08555458fa3008b80ba
EwRicklefs <ew.ricklefs@gmail.com>
2019-04-10 22:17:36 -0700

    server stuff

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "MyNewPass",
+    database: "burgers_db"
+  })
+}
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
